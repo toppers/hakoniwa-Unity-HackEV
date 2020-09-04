17314ba92ea449da2092894adfa5ce206ad546f7
tmori@esm.co.jp <tmori@esm.co.jp>
2020-09-04 14:05:31 +0900

    fix #14

--- Installer/installer/utils/ev3rt_install.bash
+++ Installer/installer/utils/ev3rt_install.bash
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ -d ev3rt-athrill-v850e2m ]
+then
+    echo "INFO: ev3rt already installed."
+    exit 0
+fi
 
 echo "### Install ev3rt"
 git clone https://github.com/toppers/ev3rt-athrill-v850e2m.git
--- Installer/installer/utils/sample_install.bash
+++ Installer/installer/utils/sample_install.bash
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ -d hakoniwa-scenario-samples ]
+then
+    echo "INFO: sample scenario already installed."
+    exit 0
+fi
 
 echo "### Install sample"
 git clone https://github.com/toppers/hakoniwa-scenario-samples.git
