ef5b5070db96102e2f7675e2a9b76ca31779adbb
tmori <tmori@esm.co.jp>
2020-10-03 16:11:10 +0900

    bug fixed for arguments and added config.json up

--- Installer/installer/start-unity.bash
+++ Installer/installer/start-unity.bash
@@ -10,12 +10,12 @@ source config/config.bash
 
 if [ $# -ge 2 ]
 then
-	export UNITY_PROJECT_NAME=${2}
+	export UNITY_PROJECT_NAME=${1}
 fi
 
 if [ $# -eq 3 ]
 then
-	export APL_NAME=${3}
+	export APL_NAME=${2}
 fi
 
 if [ -d ${UNITY_PROJECT_BUILD_PATH} ]
@@ -24,6 +24,16 @@ then
 else
     echo "ERROR: not found directory: ${UNITY_PROJECT_BUILD_PATH}"
     exit 1
+fi
+
+
+cat ${UNITY_PROJECT_BUILD_PATH}/config.json | grep Udp | grep null > /dev/null
+if [ $? -eq 0 ]
+then
+    echo "### create config.json for unity"
+    bash config/scripts/create_config.bash mmap unity
+else
+    bash config/scripts/create_config.bash udp unity
 fi
 
 cd ${UNITY_PROJECT_BUILD_PATH}
