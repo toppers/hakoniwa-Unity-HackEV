307272c170ce7fee46df587d6a044c18bdc79d48
tmori@esm.co.jp <tmori@esm.co.jp>
2020-12-05 19:23:44 +0900

    add skelton model

--- Assets/Scripts/Hakoniwa/Assets/EV3/UltrasonicSensor.cs
+++ Assets/Scripts/Hakoniwa/Assets/EV3/UltrasonicSensor.cs
@@ -8,8 +8,8 @@ namespace Hakoniwa.Assets.EV3
 {
     public class UltrasonicSensor : MonoBehaviour, IRobotUltraSonicSensor
     {
-        private float contact_distance = 250f; /* cm */
-        public float distanceValue; /* cm */
+        private float contact_distance = 30; /* centimeters/10 */
+        public float distanceValue; /* centimeters/10 */
         private GameObject frontSensor;
 
         public void Initialize(GameObject root)
@@ -20,12 +20,11 @@ namespace Hakoniwa.Assets.EV3
 
         public float GetDistanceValue()
         {
-            if ((this.distanceValue < 3) || (this.distanceValue > 249))
+            if ((this.distanceValue < 0.3) || (this.distanceValue > 24.9))
             {
                 return 255;
             }
-            //Debug.Log("distance=" + this.distanceValue);
-            return distanceValue * 1; /* centimeters */
+            return distanceValue * 10; /* centimeters */
         }
         public void UpdateSensorValues()
         {
---
+++ Assets/Scripts/Hakoniwa/Assets/HackEVSkelton/HackEVSkeltonParts.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Hakoniwa.Assets.EV3;
+
+namespace Hakoniwa.Assets.HackEV
+{
+    public class HackEVSkeltonParts : MonoBehaviour, IEV3Parts
+    {
+        private string motor_a = "LeftTire";
+        private string motor_b = "RightTire";
+        private string color_sensor = "ColorSensor/Camera";
+        private string ultra_sonic_sensor = "Axis/Body/UltraSonicSensor";
+        private string gyro_sensor = null;
+        private string motor_arm = "ArmMotor";
+        private string touch_sensor0 = null;
+        private string touch_sensor1 = "ColorSensor/FrontTouchSensor";
+        private string led = "Axis/Body/LED";
+
+        public string GetColorSensor()
+        {
+            return color_sensor;
+        }
+
+        public string GetMotorA()
+        {
+            return motor_a;
+        }
+
+        public string GetMotorB()
+        {
+            return motor_b;
+        }
+
+        public string getUltraSonicSensor()
+        {
+            return ultra_sonic_sensor;
+        }
+        public string getTouchSensor0()
+        {
+            return touch_sensor0;
+        }
+
+        public string getGyroSensor()
+        {
+            return gyro_sensor;
+        }
+
+        public string GetMotorC()
+        {
+            return motor_arm;
+        }
+
+        string IEV3Parts.getTouchSensor1()
+        {
+            return touch_sensor1;
+        }
+        public string GetLed()
+        {
+            return led;
+        }
+    }
+}
